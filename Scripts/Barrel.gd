e6bf66745b1abf86f8733c678a404d46878fc0e4
Wholffe <marc2002sch@googlemail.com>
2023-10-30 14:36:54 +0100

    add Barrel Loot

--- Scripts/Barrel.gd
+++ Scripts/Barrel.gd
@@ -11,6 +11,7 @@ func checkDestroy():
 		await  get_tree().create_timer(0.1).timeout
 		$AnimatedSprite2D.visible = false
 		await  get_tree().create_timer(0.3).timeout
+		spawnRandomItem(self.position)
 		queue_free()
 	
 # Called when the node enters the scene tree for the first time.
@@ -21,6 +22,20 @@ func _ready():
 func _process(delta):
 	pass
 
+func spawnRandomItem(targetLocation):
+	var newItem = null
+	
+	randomize()
+	var randomItemSpawner = randi_range(0,2)
+	match(randomItemSpawner):
+		1: newItem = preload("res://Actors/ColdCoin.tscn")
+		2: newItem = preload("res://Actors/RedDiamond.tscn")
+	
+	if(newItem != null):
+		var newItemInstance = newItem.instantiate()
+		newItemInstance.position = targetLocation
+		get_parent().add_child(newItemInstance)
+	
 func _on_area_2d_area_entered(area):
 	if(area.is_in_group("FromPlayer")):
 		$AnimatedSprite2D.play(("hit"))
