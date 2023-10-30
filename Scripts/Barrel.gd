edac4019405d505ff2ed2d94b5b32e177b411c35
Wholffe <marc2002sch@googlemail.com>
2023-10-30 13:26:51 +0100

    Barrel replace script origin and add animated sprite

--- Scripts/Barrel.gd
+++ Scripts/Barrel.gd
@@ -1,5 +1,7 @@
-extends Area2D
+extends Node2D
 
+@export
+var hp = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,7 +12,6 @@ func _ready():
 func _process(delta):
 	pass
 
-
-func _on_area_entered(area):
+func _on_area_2d_area_entered(area):
 	if(area.is_in_group("FromPlayer")):
-		print("player hit")
+		$AnimatedSprite2D.play(("hit"))
